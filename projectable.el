1c10799e19faca341fdadad7c89c5c20f28e6f27
Dom Charlesworth <dgc336@gmail.com>
2015-12-07 21:35:16 +0000

    Check to see if extension is ACTUALLY EXTENSION
    Despite the fact that the variable mentioned ignored EXTENSIONS it
    actually also includes potential directories IFF they end in a slash.
    
    Seriously?
    
    So I have to check to see whether it's a directory or not to build into
    the right regexp

--- projectable.el
+++ projectable.el
@@ -585,7 +585,7 @@ i.e.  If indent level was 4, the indent string would be '    '."
 
 (defun projectable-get-filter-regexps (&optional separator)
   "Flatten and concatenate all filter regexps for find command with SEPARATOR."
-  (let ((local-completion-extensions (--mapcat (list (format "%s$" it)) completion-ignored-extensions)))
+  (let ((local-completion-extensions (--mapcat (list (if (string-match "[a-z]$" it) (format "%s$" it) it)) completion-ignored-extensions)))
     (mapconcat 'regexp-quote (append local-completion-extensions projectable-filter-regexps) (or separator "|"))))
 
 ;;; Projectable Mode
